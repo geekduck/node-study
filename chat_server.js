a8609ca63726288fbf3f8a463e74e8cacb5490be
geekduck <duck1218+github@gmail.com>
2014-12-26 00:00:40 +0900

    Fix typo.

--- chat_server.js
+++ chat_server.js
@@ -1,9 +1,9 @@
 var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
-var nickNames;
+var nickNames = {};
 var namesUsed = [];
-var currentRoom;
+var currentRoom = {};
 
 exports.listen = function(server) {
   io = socketio.listen(server);
